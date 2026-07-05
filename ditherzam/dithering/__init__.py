0e9ec29de64997d6fdcebe663cb302222498333f
Arshazam <arshazamani1387@gmail.com>
2026-07-05 01:41:21 -0700

    feat(dither): special-effects family (burst, wave, contour, vortex, wireframe)

--- ditherzam/dithering/__init__.py
+++ ditherzam/dithering/__init__.py
@@ -8,3 +8,4 @@ from .kernels import error_diffusion as _error_diffusion  # noqa: E402,F401
 from .kernels import ordered as _ordered_kernels          # noqa: E402,F401
 from .kernels import pattern as _pattern_kernels          # noqa: E402,F401
 from .kernels import glitch as _glitch_kernels            # noqa: E402,F401
+from .kernels import special as _special_kernels          # noqa: E402,F401
